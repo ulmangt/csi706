dde20df92c0700cf0d2bd30b91e2a8878971059e
Geoffrey Ulman <ulman@metsci.com>
2012-09-16 18:28:30 -0400

    homework 2 problem 2 : significantly cleaned up solution

--- src/main/java/Homework2Problem2.java
+++ src/main/java/Homework2Problem2.java
@@ -16,82 +16,50 @@ public class Homework2Problem2
         new IncrementThread0( ).start( );
         new IncrementThread1( ).start( );
     }
-    
+
     // controls access to critical section
     private static countingSemaphore mutex = new countingSemaphore( 1 );
-    
+
     // semaphore for thread 0 to block on while awaiting its turn
+    // ** starts at 1 to indicate that it is thread 0's turn initially **
     private static countingSemaphore turn_queue_0 = new countingSemaphore( 1 );
-    
+
     // semaphore for thread 1 to block on while awaiting its turn
     private static countingSemaphore turn_queue_1 = new countingSemaphore( 0 );
-    
-    // turn counter indicating which thread
-    // should proceed into the critical section next
-//    private static int turn = 0;
-    
-    private static boolean waiting_0 = false;
-    private static boolean waiting_1 = false;
-    
+
     // counter incremented by multiple threads
     private static int s = 0;
-    
+
     public static class IncrementThread0 extends Thread
     {
         @Override
         public void run( )
         {
             while ( true )
-            {                
-                // start entry section
-//                mutex.P( );
-                
-//                waiting_0 = true;
-                
-                // release mutex so that we don't block
-                // on turn_queue.P( ) holding mutex lock
-//                mutex.V( );
-                
+            {
                 // wait outside of the critical section for our turn
                 // this violates progress, but this is inevitable
                 // because we are specifically required to have the
                 // threads take turns
-                //
-                // consider possible VP bug:
-                // if a context switch occurs here
                 turn_queue_0.P( );
-                
-                // re-acquire mutex to decrement waiting counter
-//                mutex.P( );
-                
-//                waiting_0 = false;
-                
-                // end entry section
-//                mutex.V( );
-                
+
                 // start critical section
                 mutex.P( );
-                
+
                 // increment s inside critical section
                 s = s + 1;
-                
-                // set turn inside critical section
-//                turn = 1;
-                
-                // notify Thread 1 that it is their turn
-//                if ( waiting_1 )
-                {
-                    turn_queue_1.V( );
-                }
+
+                // notify thread 1 that it is their turn
+                turn_queue_1.V( );
 
                 System.out.printf( "Thread 0 takes turn (s = %d)%n", s );
-                
+
                 // end critical section
                 mutex.V( );
             }
         }
     }
-    
+
     public static class IncrementThread1 extends Thread
     {
         @Override
@@ -99,49 +67,23 @@ public class Homework2Problem2
         {
             while ( true )
             {
-                // start entry section
-//              mutex.P( );
-              
-//              waiting_1 = true;
-              
-              // release mutex so that we don't block
-              // on turn_queue.P( ) holding mutex lock
-//              mutex.V( );
-              
-              // wait outside of the critical section for our turn
-              // this violates progress, but this is inevitable
-              // because we are specifically required to have the
-              // threads take turns
-              //
-              // consider possible VP bug:
-              // if a context switch occurs here
-              turn_queue_1.P( );
-              
-              // re-acquire mutex to decrement waiting counter
-//              mutex.P( );
-              
-//              waiting_1 = false;
-              
-              // end entry section
-//              mutex.V( );
-                
+                // wait outside of the critical section for our turn
+                // this violates progress, but this is inevitable
+                // because we are specifically required to have the
+                // threads take turns
+                turn_queue_1.P( );
+
                 // start critical section
                 mutex.P( );
-                
+
                 // increment s inside critical section
                 s = s + 1;
-                
-                // set turn inside critical section
-//                turn = 0;
-                
-                // notify Thread 0 that it is their turn
-//                if ( waiting_0 )
-                {
-                    turn_queue_0.V( );
-                }
+
+                // notify thread 0 that it is their turn
+                turn_queue_0.V( );
 
                 System.out.printf( "Thread 1 takes turn (s = %d)%n", s );
-                
+
                 // end critical section
                 mutex.V( );
             }
